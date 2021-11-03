6269791ee0ae4670d6735e7571c77bae8d49d693
Rémi Monthiller <monthir@git.inpt.fr>
2021-11-03 14:26:00 +0100

    helper scripts to copy initializers and post processing

---
+++ app/interface/init/objects/copy_inits.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+# Command line program
+import argparse
+import argcomplete
+# Name check
+import re
+# Directory operations
+import shutil
+import os
+# Replace operations
+import fileinput
+# Custom Libraries
+import sys
+script_dir = os.path.dirname(os.path.realpath(__file__))
+lib_directory = script_dir[:script_dir.find("interface") + len("interface")]
+sys.path.append(lib_directory)
+import libchoose
+
+def parse():
+    # parser
+    parser = argparse.ArgumentParser(description='copy all initializers of an object to another object')
+    parser.add_argument('source', help='specify the name of source object')
+    parser.add_argument('dest', help='specify the name of the destination object')
+    # autocompletion
+    argcomplete.autocomplete(parser)
+    # parse
+    return parser.parse_args()
+
+def edit(source, name):
+    upper_camel_name = libchoose.object_to_upper_camel_case([name])
+    # init
+    ## replace source by name
+    libchoose.edit_choice(name, [source], [name])
+    for line in fileinput.FileInput(name + "/parameters.h", inplace=True):
+        print(line.replace('name = "{}"'.format(source), 'name = "{}"'.format(name)), end='')
+
+def main(source, name):
+    name_check = re.compile('[][@!#$%^&*()<>?/\|}{~:+.\'"]')
+    if(name_check.search(name) == None):
+        if os.path.exists(name):
+            # remove
+            shutil.rmtree(name)
+            # copy
+            shutil.copytree(source, name, symlinks=True)
+            # edit
+            edit(source, name)
+        else:
+            print("ERROR: destination object not found")
+    else:
+        print("ERROR: name shouldn't contain special characters")
+
+if __name__ == '__main__':
+    # main
+    args = parse()
+    main(args.source, args.dest)
