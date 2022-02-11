9dfd3ee83b6eed55505c5ed164baddca19554423
Rémi Monthiller <remi.monthiller@gmail.com>
2022-02-11 12:27:32 +0100

    corrected channel flow cases

--- cases/adaptive_surfers_in_channel_flow/param/run/parameters.h
+++ cases/adaptive_surfers_in_channel_flow/param/run/parameters.h
@@ -10,10 +10,10 @@ namespace c0p {
 struct RunParameters {
     // Fixed time step of the simulation.
     // Should be greater than zero.
-    double dt = 0.002;
+    double dt = 0.0065;
     // Number of time steps of the simulation.
     // Should be greater than zero.
-    std::size_t nt = std::round(10.0 / dt);
+    std::size_t nt = std::round(25.9 / dt);
     // Number of time steps that will be saved.
     // Currently, post processing will only be
     // possible to be performed on saved time steps.
