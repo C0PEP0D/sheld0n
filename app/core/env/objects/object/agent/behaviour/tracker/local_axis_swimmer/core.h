d43b23b8064728f76b609291150472447fd470d0
Rémi Monthiller <remi.monthiller@gmail.com>
2022-04-25 10:25:09 +0200

    checking error with comparison

--- app/core/env/objects/object/agent/behaviour/tracker/local_axis_swimmer/core.h
+++ app/core/env/objects/object/agent/behaviour/tracker/local_axis_swimmer/core.h
@@ -10,44 +10,44 @@ namespace c0p {
 
 template<typename TypeParameters, typename AgentActiveStep, typename TypeSensorPosition, typename TypeSensorVelocityGradients, typename TypeBehaviourDirection, typename TypeBehaviourVelocity>
 class AgentBehaviourTrackerLocalAxisSwimmer : public sl0::sa0::Behaviour<AgentActiveStep> {
-    public:
-        using Type = sl0::sa0::Behaviour<AgentActiveStep>;
-        using TypeStateVectorDynamic = typename AgentActiveStep::TypeStateVectorDynamic;
-    public:
-        TypeParameters parameters;
-        // sensors
-        TypeSensorPosition sensorPosition;
-        TypeSensorVelocityGradients sensorVelocityGradients;
-        // sub behaviours
-        TypeBehaviourDirection behaviourDirection;
-        TypeBehaviourVelocity behaviourVelocity;
-    public:
-        AgentBehaviourTrackerLocalAxisSwimmer(std::shared_ptr<Objects> sObjects) : sensorPosition(sObjects) {
-        }
-    public:
-        TypeContainer<TypeSpaceVector> positions(const double* pState, const AgentActiveStep& stepActive) const override {
-            TypeContainer<TypeSpaceVector> result;
-            // sensors
-            const TypeContainer<TypeSpaceVector> positionsSensorPosition = sensorPosition.positions(pState, stepActive);
-            result.insert(result.end(), positionsSensorPosition.begin(), positionsSensorPosition.end());
-            const TypeContainer<TypeSpaceVector> positionsSensorVelocityGradients = sensorVelocityGradients.positions(pState, stepActive);
-            result.insert(result.end(), positionsSensorVelocityGradients.begin(), positionsSensorVelocityGradients.end());
-            // sub behaviours
-            const TypeContainer<TypeSpaceVector> positionsBehaviourDirection = behaviourDirection.positions(pState, stepActive);
-            result.insert(result.end(), positionsBehaviourDirection.begin(), positionsBehaviourDirection.end());
-            const TypeContainer<TypeSpaceVector> positionsBehaviourVelocity = behaviourVelocity.positions(pState, stepActive);
-            result.insert(result.end(), positionsBehaviourVelocity.begin(), positionsBehaviourVelocity.end());
-            // return
-            return result;
-        }
-    public:
-        void operator()(const double* pState, const double& t, const AgentActiveStep& stepActive) const override {
-            const TypeSpaceVector direction = sensorPosition(pState, t, stepActive);
-            const TypeSpaceMatrix velocityGradients = sensorVelocityGradients(pState, t, stepActive);
-            const TypeSpaceVector swimmingDirection = stepActive.cAxis(pState);
-            stepActive.sStepAxisOrient->direction = behaviourDirection(pState, t, stepActive, direction, velocityGradients).normalized();
-            stepActive.sStepLocalAxisSwim->velocity = behaviourVelocity(pState, t, stepActive, direction, velocityGradients, swimmingDirection) * stepActive.parameters.velocity;
-        }
+	public:
+		using Type = sl0::sa0::Behaviour<AgentActiveStep>;
+		using TypeStateVectorDynamic = typename AgentActiveStep::TypeStateVectorDynamic;
+	public:
+		TypeParameters parameters;
+		// sensors
+		TypeSensorPosition sensorPosition;
+		TypeSensorVelocityGradients sensorVelocityGradients;
+		// sub behaviours
+		TypeBehaviourDirection behaviourDirection;
+		TypeBehaviourVelocity behaviourVelocity;
+	public:
+		AgentBehaviourTrackerLocalAxisSwimmer(std::shared_ptr<Objects> sObjects) : sensorPosition(sObjects) {
+		}
+	public:
+		TypeContainer<TypeSpaceVector> positions(const double* pState, const AgentActiveStep& stepActive) const override {
+			TypeContainer<TypeSpaceVector> result;
+			// sensors
+			const TypeContainer<TypeSpaceVector> positionsSensorPosition = sensorPosition.positions(pState, stepActive);
+			result.insert(result.end(), positionsSensorPosition.begin(), positionsSensorPosition.end());
+			const TypeContainer<TypeSpaceVector> positionsSensorVelocityGradients = sensorVelocityGradients.positions(pState, stepActive);
+			result.insert(result.end(), positionsSensorVelocityGradients.begin(), positionsSensorVelocityGradients.end());
+			// sub behaviours
+			const TypeContainer<TypeSpaceVector> positionsBehaviourDirection = behaviourDirection.positions(pState, stepActive);
+			result.insert(result.end(), positionsBehaviourDirection.begin(), positionsBehaviourDirection.end());
+			const TypeContainer<TypeSpaceVector> positionsBehaviourVelocity = behaviourVelocity.positions(pState, stepActive);
+			result.insert(result.end(), positionsBehaviourVelocity.begin(), positionsBehaviourVelocity.end());
+			// return
+			return result;
+		}
+	public:
+		void operator()(const double* pState, const double& t, const AgentActiveStep& stepActive) const override {
+			const TypeSpaceVector position = sensorPosition(pState, t, stepActive);
+			const TypeSpaceMatrix velocityGradients = sensorVelocityGradients(pState, t, stepActive);
+			const TypeSpaceVector swimmingDirection = stepActive.cAxis(pState);
+			stepActive.sStepAxisOrient->direction = behaviourDirection(pState, t, stepActive, position, velocityGradients).normalized();
+			stepActive.sStepLocalAxisSwim->velocity = behaviourVelocity(pState, t, stepActive, position, velocityGradients, swimmingDirection) * stepActive.parameters.velocity;
+		}
 };
 
 
--- app/default/param/env/objects/static/object/__agent/_behaviour/__tracker/_behaviour_direction/__custom/parameters.h
+++ app/default/param/env/objects/static/object/__agent/_behaviour/__tracker/_behaviour_direction/__custom/parameters.h
@@ -15,95 +15,103 @@ struct ObjectAgentBehaviourTrackerBehaviourDirectionCustomParameters {
 	const double vorticityThreshold1 = 1.0 / Parameters::cTime;
 	// Function that returns the preferred swimming direction
 	TypeSpaceVector operator()(const double* pState, const double& t, const AgentActiveStep&  stepActive, const TypeSpaceVector& position, const TypeSpaceMatrix& velocityGradients) const {
-		const double vorticityThreshold0 = 1.0 / Parameters::cTime / 3.0;
-    	const double vorticityThreshold1 = 1.0 / Parameters::cTime;
-    	// Function that returns the preferred swimming direction
-    	TypeSpaceVector operator()(const double* pState, const double& t, const AgentActiveStep&  stepActive, const TypeSpaceVector& position, const TypeSpaceMatrix& velocityGradients) const {
-    		// Implements the behaviour of Alageshan et al 2020.
-    		// extract vorticity from gradients
-    		const TypeSpaceMatrix skewVelocityGradients = 0.5 * (velocityGradients - velocityGradients.transpose());
-    		const TypeSpaceVector vorticity = 2.0 * TypeSpaceVector({-skewVelocityGradients(1, 2), skewVelocityGradients(0, 2), -skewVelocityGradients(0, 1)});
-    		// build cartesian system
-    		const TypeSpaceVector targetDirection = (position - stepActive.cX(pState)).normalized();
-    		const TypeSpaceVector targetDirectionCrossVorticity = targetDirection.cross(vorticity).normalized();
-    		const TypeSpaceVector targetDirectionPerp = targetDirection.cross(targetDirectionCrossVorticity).normalized();
-    		// extract orientation axis
-    		const TypeSpaceVector orientationAxis = stepActive.cAxis(pState);
-    		// if no error
-    		//const double x = orientationAxis.dot(targetDirectionPerp);
-    		//const double y = orientationAxis.dot(-targetDirectionCrossVorticity);
-    		//const double z = orientationAxis.dot(targetDirection);
-    		// if error
-    		const double x = orientationAxis.dot(targetDirectionCrossVorticity);
-    		const double y = orientationAxis.dot(targetDirectionPerp);
-    		const double z = orientationAxis.dot(targetDirection);
-    		// extract the three scalars that define the state
-    		const double vorticityNorm = vorticity.norm();
-    		const double theta = std::atan(z / std::sqrt(x*x + y*y + z*z));
-    		const double phi = std::atan2(y, x);
-    		// define actions
-    		std::vector<TypeSpaceVector> actions({
-    			targetDirection,
-    			-targetDirection,
-    			targetDirectionCrossVorticity,
-    			-targetDirectionCrossVorticity,
-    			targetDirectionPerp,
-    			-targetDirectionPerp
-    		});
-    		// compute state
-    		int state = 0;
-    		if(vorticityNorm < vorticityThreshold0) {
-    			state = 1;
-    		} else if (vorticityNorm <= vorticityThreshold1) {
-    			if(theta < M_PI / 6.0) {
-    				state = 2;
-    			} else if (theta <= 5.0 / 6.0 * M_PI) {
-    				if(phi < M_PI / 2.0) {
-    					state = 6;
-    				} else if(phi < M_PI) {
-    					state = 7;
-    				} else if(phi < 3.0/2.0 * M_PI) {
-    					state = 8;
-    				} else {
-    					state = 9;
-    				}
-    			} else {
-    				state = 3;
-    			}
-    		} else {
-    			if(theta < M_PI / 6.0) {
-    				state = 4;
-    			} else if (theta <= 5.0 / 6.0 * M_PI) {
-    				if(phi < M_PI / 2.0) {
-    					state = 10;
-    				} else if(phi < M_PI) {
-    					state = 11;
-    				} else if(phi < 3.0/2.0 * M_PI) {
-    					state = 12;
-    				} else {
-    					state = 13;
-    				}
-    			} else {
-    				state = 5;
-    			}
-    		}
-    		// return action corresponding to state
-    		switch (state) {
-    	        case 1: return actions[0];
-    	        case 2: return actions[0];
-    	        case 3: return actions[0];
-    	        case 4: return actions[5];
-    	        case 5: return actions[0];
-    	        case 6: return actions[0];
-    	        case 7: return actions[5];
-    	        case 8: return actions[4];
-    	        case 9: return actions[0];
-    	        case 10: return actions[2];
-    	        case 11: return actions[0];
-    	        case 12: return actions[2];
-    	        case 13: return actions[0];
-    	    }
-    	}
+		// Implements the behaviour of Alageshan et al 2020.
+		// extract vorticity from gradients
+		const TypeSpaceMatrix skewVelocityGradients = 0.5 * (velocityGradients - velocityGradients.transpose());
+		const TypeSpaceVector vorticity = (TypeSpaceVector() << -2.0 * skewVelocityGradients(1, 2), 2.0 * skewVelocityGradients(0, 2), -2.0 * skewVelocityGradients(0, 1)).finished();
+		// build cartesian system
+		const TypeSpaceVector targetDirection = (position - stepActive.cX(pState)).normalized();
+		const TypeSpaceVector targetDirectionCrossVorticity = targetDirection.cross(vorticity).normalized();
+		const TypeSpaceVector targetDirectionPerp = targetDirection.cross(targetDirectionCrossVorticity).normalized();
+		// extract orientation axis
+		const TypeSpaceVector orientationAxis = stepActive.cAxis(pState);
+		// if no error
+		//const double x = orientationAxis.dot(targetDirectionPerp);
+		//const double y = orientationAxis.dot(-targetDirectionCrossVorticity);
+		//const double z = orientationAxis.dot(targetDirection);
+		// if error
+		const double x = orientationAxis.dot(targetDirectionCrossVorticity);
+		const double y = orientationAxis.dot(targetDirectionPerp);
+		const double z = orientationAxis.dot(targetDirection);
+		// extract the three scalars that define the state
+		const double vorticityNorm = vorticity.norm();
+		const double theta = std::atan(z / std::sqrt(x*x + y*y + z*z));
+		const double phi = std::atan2(y, x);
+		// define actions
+		const TypeSpaceVector action0 = targetDirection;
+		const TypeSpaceVector action1 = -targetDirection;
+		const TypeSpaceVector action2 = targetDirectionCrossVorticity;
+		const TypeSpaceVector action3 = -targetDirectionCrossVorticity;
+		const TypeSpaceVector action4 = targetDirectionPerp;
+		const TypeSpaceVector action5 = -targetDirectionPerp;
+		// compute state
+		int state = 0;
+		if(vorticityNorm < vorticityThreshold0) {
+			state = 1;
+		} else if (vorticityNorm <= vorticityThreshold1) {
+			if(theta < M_PI / 6.0) {
+				state = 2;
+			} else if (theta <= 5.0 / 6.0 * M_PI) {
+				if(phi < M_PI / 2.0) {
+					state = 6;
+				} else if(phi < M_PI) {
+					state = 7;
+				} else if(phi < 3.0/2.0 * M_PI) {
+					state = 8;
+				} else {
+					state = 9;
+				}
+			} else {
+				state = 3;
+			}
+		} else {
+			if(theta < M_PI / 6.0) {
+				state = 4;
+			} else if (theta <= 5.0 / 6.0 * M_PI) {
+				if(phi < M_PI / 2.0) {
+					state = 10;
+				} else if(phi < M_PI) {
+					state = 11;
+				} else if(phi < 3.0/2.0 * M_PI) {
+					state = 12;
+				} else {
+					state = 13;
+				}
+			} else {
+				state = 5;
+			}
+		}
+		// return action corresponding to state
+		if(state == 1) {
+			return action0;
+		} else if(state == 2) {
+			return action0;
+		} else if(state == 3) {
+	   		return action0;
+	   	} else if(state == 4) {
+	   		return action0;
+	   	} else if(state == 5) {
+	   		return action0;
+	   	} else if(state == 6) {
+	   		return action0;
+	   	} else if(state == 7) {
+			return action0;
+		} else if(state == 8) {
+	   		return action0;
+	   	} else if(state == 9) {
+	   		return action0;
+	   	} else if(state == 10) {
+	   		return action0;
+	   	} else if(state == 11) {
+	   		return action0;
+	   	} else if(state == 12) {
+	   		return action0;
+	   	} else if(state == 13) {
+	   		return action0;
+	   	} else {
+	   		std::cout << "Shouldn't happen" << std::endl;
+	   		return action0;
+	   	}
 	}
 };
 
