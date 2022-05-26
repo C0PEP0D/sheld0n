13d9bea51d4b0b853d98ec3b1eb4fcffe7dd31ac
Rémi Monthiller <remi.monthiller@gmail.com>
2022-05-26 17:13:08 +0200

    post and updated TODO list

--- cases/TODO.md
+++ cases/TODO.md
@@ -2,15 +2,26 @@
 
 ## CASES:
 
-* RUNNING: risers
-* RUNNING: trackers_fixed_target (rename to straight_trackers_fixed_target) -> TO RUN: surfing_trackers_fixed_target
+* RE RUNNING: straight_trackers_fixed_target
+* RUNNING: surfing_trackers_fixed_target
 * TO RUN: straight_trackers_passive_target
-* TO RUN: straight_trackers_passive_target
-* TO RUN: passive clever all
-* TO RUN: partial all
-* TO RUN: filtering
+* TO RUN: surfing_trackers_passive_target
+* RUNNING: intertial_risers
+* RUNNING: partial_surfers
+* TO RUN: negative_surfers
+* TO RUN: sym_dir_surfers
+* TO RUN: grad_dir_surfers
+* RUNNING: bottom_heavy_spherical_swimmers
+* TO RUN: bottom_heavy_spheroidal_swimmers
+* RUNNING: filtering_surfers
+* RUNNING: exp_surfers
+* TO RUN: on_off_surfers
+* TO RUN: hessian_surfers
 
 ## POST:
 
-* TO CHECK: tracers post_tracers
-* TO RUN: risers dispersion
+* RUNNING: risers
+* RUNNING: tracers post_tracers
+* RUNNING: surfers post_velocity_axis
+
+## DONE:
--- modules/pyp0st/post_dispersion_plane.py
+++ modules/pyp0st/post_dispersion_plane.py
@@ -16,12 +16,11 @@ def fit_func(x, a, b, c, d):
 
 def parse():
     parser = argparse.ArgumentParser(description='Computes the average velocity along a specific direction')
-    parser.add_argument('normal', nargs='?', type=int, default=0, help='specify the plane normal axis')
     parser.add_argument('--number-average', '-n', nargs='?', type=int, default=30, help='number of time steps for wich average quantities are computed, default: 30')
     return parser.parse_args()
 
-def main(normal, n_average):
-    print("INFO: Post processing the dispersion in the plane of normal {normal}.".format(normal=normal), flush=True)
+def main(n_average):
+    print("INFO: Post processing the dispersion.", flush=True)
     print("INFO: Reading objects...", flush=True)
     object_names = libpost.get_object_names()
     print("INFO: Done. Object names are:", " ".join(object_names), flush=True)
@@ -34,27 +33,17 @@ def main(normal, n_average):
     objects_pos_init = {}
     for name in object_names:
         obj = libpost.get_object(time_dirs[time_list.index(0.0)], name)
-        if normal == 0:
-            pos_1 = libpost.get_object_properties(obj, ["particle_.*__pos_1"])["value"]
-            objects_pos_init[name] = np.empty((pos_1.size, 2))
-            objects_pos_init[name][:, 0] = pos_1
-            objects_pos_init[name][:, 1] = libpost.get_object_properties(obj, ["particle_.*__pos_2"])["value"]
-        elif normal == 1:
-            pos_2 = libpost.get_object_properties(obj, ["particle_.*__pos_2"])["value"]
-            objects_pos_init[name] = np.empty((pos_2.size, 2))
-            objects_pos_init[name][:, 0] = pos_2
-            objects_pos_init[name][:, 1] = libpost.get_object_properties(obj, ["particle_.*__pos_0"])["value"]
-        elif normal == 2:
-            pos_0 = libpost.get_object_properties(obj, ["particle_.*__pos_0"])["value"]
-            objects_pos_init[name] = np.empty((pos_0.size, 2))
-            objects_pos_init[name][:, 0] = pos_0
-            objects_pos_init[name][:, 1] = libpost.get_object_properties(obj, ["particle_.*__pos_1"])["value"]
+        pos_0 = libpost.get_object_properties(obj, ["particle_.*__pos_0"])["value"]
+        objects_pos_init[name] = np.empty((pos_0.size, 3))
+        objects_pos_init[name][:, 0] = pos_0
+        objects_pos_init[name][:, 1] = libpost.get_object_properties(obj, ["particle_.*__pos_1"])["value"]
+        objects_pos_init[name][:, 2] = libpost.get_object_properties(obj, ["particle_.*__pos_2"])["value"]
     print("INFO: Done.", flush=True)
     print("INFO: Processing effective velocity along axis...", flush=True)
     objects_standard_deviation = {}
     for name in object_names:
         print("\tINFO: Processing object {name}...".format(name=name), flush=True)
-        objects_standard_deviation[name] = np.empty((average_time_steps.shape[0], 2))
+        objects_standard_deviation[name] = np.empty((average_time_steps.shape[0], 3))
         for index in range(average_time_steps.size):
             t = time[average_time_steps[index]]
             print("\t\tINFO: Processing t={t}/{t_f}...".format(t=t, t_f=time[-1]), flush=True)
@@ -62,24 +51,18 @@ def main(normal, n_average):
             obj = libpost.get_object(time_dirs[time_list.index(t)], name)
             # effective velocity
             pos = np.empty(objects_pos_init[name].shape)
-            if normal == 0:
-                pos[:, 0] = libpost.get_object_properties(obj, ["particle_.*__pos_1"])["value"]
-                pos[:, 1] = libpost.get_object_properties(obj, ["particle_.*__pos_2"])["value"]
-            elif normal == 1:
-                pos[:, 0] = libpost.get_object_properties(obj, ["particle_.*__pos_2"])["value"]
-                pos[:, 1] = libpost.get_object_properties(obj, ["particle_.*__pos_0"])["value"]
-            elif normal == 2:
-                pos[:, 0] = libpost.get_object_properties(obj, ["particle_.*__pos_0"])["value"]
-                pos[:, 1] = libpost.get_object_properties(obj, ["particle_.*__pos_1"])["value"]
+            pos[:, 0] = libpost.get_object_properties(obj, ["particle_.*__pos_0"])["value"]
+            pos[:, 1] = libpost.get_object_properties(obj, ["particle_.*__pos_1"])["value"]
+            pos[:, 2] = libpost.get_object_properties(obj, ["particle_.*__pos_2"])["value"]
             objects_standard_deviation[name][index, :] = np.std(pos - objects_pos_init[name], axis=0)
             print("\t\tINFO: Done processing t={t}/{t_f}.".format(t=t, t_f=time[-1]), flush=True)
         print("\tDone processing object {name}.".format(name=name), flush=True)
     print("INFO: Done.", flush=True)
     print("INFO: Saving...", flush=True)
     for name in object_names:
-        np.savetxt("{name}__standard_deviation_normal_to_{normal}.csv".format(name=name, normal=normal), objects_standard_deviation[name], delimiter=",", header="time,std_x,std_y")
+        np.savetxt("{name}__standard_deviation.csv".format(name=name), np.column_stack((time[average_time_steps], objects_standard_deviation[name])), delimiter=",", header="time,std_x,std_y,std_z")
     print("INFO: Done.", flush=True)
 
 if __name__ == '__main__':
     args = parse()
-    main(args.normal, args.number_average)
+    main(args.number_average)
