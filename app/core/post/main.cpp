d05dc7d4cc434073d024bff797ade304390dc31d
Rémi Monthiller <monthir@git.inpt.fr>
2024-11-15 17:54:33 +0100

    fix: fixed compilation of post processing

--- app/core/post/main.cpp
+++ app/core/post/main.cpp
@@ -1,13 +1,13 @@
 // app includes
 // // env
-#include "core/core.h"
 #include "param/parameters.h"
+#include "core/core.h"
 // // solutions
-#include "core/solutions/core.h"
 #include "param/solutions/parameters.h"
+#include "core/solutions/core.h"
 // // post
-#include "core/post/core.h"
 #include "param/post/parameters.h"
+#include "core/post/core.h"
 
 int main () { 
     c0p::Post<c0p::PostParameters> post;
