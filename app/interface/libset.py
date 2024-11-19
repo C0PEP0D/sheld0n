6ff2d9bb056fe3b44525923c478e3a2a73351bc9
Rémi Monthiller <monthir@git.inpt.fr>
2024-11-19 08:04:08 +0100

    feat: added global parameter setting feature

---
+++ app/interface/libset.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+# command line program
+import argparse
+# directory operations
+import os
+# file edit
+import fileinput
+import fnmatch
+# regular expression
+import re
+
+def parse():
+    parser = argparse.ArgumentParser(description='Parameter library that contains functions to set the parameters of the simulation.')
+    return parser.parse_args()
+
+def find_replace(folder, file_pattern, text, replacement):
+    for dirpath, dirs, files in os.walk(folder, topdown=True):
+        files = [os.path.join(dirpath, filename) for filename in fnmatch.filter(files, file_pattern)]
+        if files:
+            for file in files:
+                lineno = 1
+                data_array = []
+                for line in fileinput.FileInput(file, inplace=True):
+                    text_match = re.search(text, line)
+                    if text_match:
+                        print(re.sub(text, replacement, line), end='')
+                        data_array.append({
+                            "text":text_match[0], 
+                            "replacement":replacement,
+                            "lineno":lineno
+                        })
+                    else:
+                        print(line, end='')
+                    lineno += 1
+                if data_array:
+                    for data in data_array:
+                        print("INFO: {file}:{lineno}: {text} ---> {replacement}".format(file=file, **data))
+
+def set_parameter(dest, param, value):
+    find_replace(dest, "parameters.h", r" {param} = [^\;]*;".format(param=param), " {param} = {value};".format(param=param, value=str(value)))
+
+if __name__ == '__main__':
+    parse()
