dbe821cb9c426708fa02c45e5e201b67df1b399f
Rémi Monthiller <monthir@git.inpt.fr>
2024-11-22 17:21:02 +0100

    feat: more flows

---
+++ app/default/param/flow/__abc.h
@@ -0,0 +1,36 @@
+#ifndef C0P_PARAM_FLOW_PARAMETERS_H
+#define C0P_PARAM_FLOW_PARAMETERS_H
+#pragma once
+
+namespace c0p {
+
+struct Flow {
+
+	static constexpr double A = 1.0;
+	static constexpr double B = 1.0;
+	static constexpr double C = 1.0;
+
+	static tSpaceVector getVelocity(const double* pX, const double t) {
+		return tSpaceVector({
+				A * std::sin(pX[2]) + C * std::cos(pX[1]),
+				B * std::sin(pX[0]) + A * std::cos(pX[2]),
+				C * std::sin(pX[1]) + B * std::cos(pX[0])
+		});
+	};
+
+	static tSpaceMatrix getVelocityGradients(const double* pX, const double t) {
+		tSpaceMatrix velocityGradients = tSpaceMatrix::Zero();
+		velocityGradients(0,0) = 0.0;                  velocityGradients(0,1) = C * -std::sin(pX[1]); velocityGradients(0,2) = A * std::cos(pX[2]);
+		velocityGradients(1,0) = B * std::cos(pX[0]);  velocityGradients(1,1) = 0.0;                  velocityGradients(1,2) = A * -std::sin(pX[2]);
+		velocityGradients(2,0) = B * -std::sin(pX[0]); velocityGradients(2,1) = C * std::cos(pX[1]);  velocityGradients(2,2) = 0.0;
+		return velocityGradients;
+	};
+	
+	static tSpaceVector getAcceleration(const double* pX, const double t) {
+		return tSpaceVector::Zero();
+	};
+};
+
+}
+
+#endif
---
+++ app/default/param/flow/__linear3d.h
@@ -0,0 +1,50 @@
+#ifndef C0P_PARAM_FLOW_PARAMETERS_H
+#define C0P_PARAM_FLOW_PARAMETERS_H
+#pragma once
+
+// std includes
+#include <array>
+
+namespace c0p {
+
+struct Flow {
+
+	static constexpr std::array<double, DIM> Velocity0 = {
+		1.0, 
+		1.0,
+		1.0
+	};
+	static constexpr std::array<double, DIM * DIM> Gradient = {
+		1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, -2.0
+	};
+	static constexpr std::array<double, DIM> Acceleration = {
+		0.0, 
+		0.0,
+		0.0
+	};
+
+	static tSpaceVector getVelocity(const double* pX, const double t) {
+		// parameters
+		const tView<const tSpaceVector> u0(Velocity0.data());
+		const tView<const tSpaceMatrix> grad(Gradient.data());
+		const tView<const tSpaceVector> a(Acceleration.data());
+		// input
+		const tView<const tSpaceVector> x(pX);
+		// output
+		return u0 + grad * x + a * t;
+	};
+
+	static tSpaceMatrix getVelocityGradients(const double* pX, const double t) {
+		return tView<const tSpaceMatrix>(Gradient.data());
+	};
+	
+	static tSpaceVector getAcceleration(const double* pX, const double t) {
+		return tView<const tSpaceVector>(Acceleration.data());
+	};
+};
+
+}
+
+#endif
