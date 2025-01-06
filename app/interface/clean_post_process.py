1ba872ea7cb17574e2f8a9fc340f7b91a3d759d3
Rémi Monthiller <monthir@git.inpt.fr>
2025-01-06 18:28:55 +0100

    feat: improved cleaning scripts

--- app/interface/clean_post_process.py
+++ app/interface/clean_post_process.py
@@ -8,17 +8,13 @@ import shutil
 
 
 def parse():
-    parser = argparse.ArgumentParser(description='Clean the case directory. Deletes build and time directories.')
+    parser = argparse.ArgumentParser(description='Clean the case directory. Deletes the time directory.')
     return parser.parse_args()
 
 def main():
     args = parse()
-    if os.path.exists('build'):
-        shutil.rmtree('build')
     if os.path.exists('time'):
         shutil.rmtree('time')
-    if os.path.exists('post_process/time'):
-        shutil.rmtree('post_process/time')
 
 if __name__ == '__main__':
     main()
